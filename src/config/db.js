d2c025dd945a5d2b6c29767b5be23c1ed06d3ce0
ericcampos32398075 <ericaguiar_@hotmail.com>
2024-08-14 16:58:13 -0300

    refactor: alter path

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
-const dbUser = "";
-const dbPassword = "";
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
     try {
         const dbConn = await mongoose.connect(
-            `mongodb+srv://ericcampos:<password>@cluster0.8wvwwc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8wvwwc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
         );
-        console.log('conectou ao banco')
+        console.log('conectou ao banco');
     } catch (error) {
-        console.log*(error)
+        console.log(error);
     }
 }
 
+conn();
 
 module.exports = conn;
