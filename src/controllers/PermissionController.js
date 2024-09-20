b7b3da4ec14e35b49b5c25e633384f63fc859d1c
ericcampos32398075 <ericaguiar_@hotmail.com>
2024-09-19 22:47:02 -0300

    feacture/PermissionController

---
+++ src/controllers/PermissionController.js
@@ -0,0 +1,22 @@
+const { Permission } = require("../models");
+
+// Create a new permission
+const createPermission = async (req, res) => {
+
+  try {
+    const { role, description, permissions } = req.body;
+    const newPermission = await Permission.create({ role, description, permissions });
+
+    if (!newPermission) {
+      res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde"] });
+      return;
+    }
+
+    res.status(201).json(newPermission);
+
+  } catch (error) {
+    res.status(500).json({ errors: ["Erro no servidor, tente novamente mais tarde!"] });
+  }
+};
+
+// Get all permissions
---
+++ src/models/Permission.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const PermissionSchema = new mongoose.Schema({
+
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "assistant", "user"],
+    default: "user"
+  },
+  description: {
+    type: [String],
+    required: true
+  },
+  permissions: {
+    type: [String],
+    required: true
+  },
+  createdAt: {
+    type: date,
+    default: Date.now
+  }
+});
+
+const Permission = mongoose.model("Permission", PermissionSchema);
+
+module.exports = Permission;
